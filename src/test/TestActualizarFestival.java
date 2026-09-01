cbfea61ce97d052c3af179d163f9e14181ba635b
valentinaponzo <ponzo.valentinaa@gmail.com>
2026-09-01 01:57:29 -0300

    Agrega casos de uso de consulta y  una actualización

---
+++ src/test/TestActualizarFestival.java
@@ -0,0 +1,39 @@
+package test;
+
+import java.time.LocalDate;
+
+import datos.Festival;
+import negocios.FestivalABM;
+
+public class TestActualizarFestival {
+
+    public static void main(String[] args) {
+
+        FestivalABM abm = new FestivalABM();
+
+        try {
+
+            // Traemos de la BD el festival con id 1
+            Festival festival = abm.traer(1L);
+
+            System.out.println("ANTES:");
+            System.out.println(festival);
+
+            // Modificamos algunos datos
+            festival.setTemporada("primavera");
+            festival.setFechaFin(LocalDate.of(2026, 10, 15));
+
+            // Guardamos los cambios en la BD
+            abm.modificar(festival);
+
+            // Lo volvemos a traer para comprobar
+            Festival festivalActualizado = abm.traer(1L);
+
+            System.out.println("DESPUES:");
+            System.out.println(festivalActualizado);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/TestConsultaFestivalUnidades.java
@@ -0,0 +1,33 @@
+package test;
+
+import datos.Festival;
+import datos.UnidadVenta;
+import negocios.FestivalABM;
+
+public class TestConsultaFestivalUnidades {
+
+    public static void main(String[] args) {
+
+        FestivalABM abm = new FestivalABM();
+
+        try {
+
+            long idFestival = 1;
+
+            Festival festival =
+                    abm.traerFestivalYUnidadesDeVenta(idFestival);
+
+            System.out.println("FESTIVAL:");
+            System.out.println(festival);
+
+            System.out.println("\nUNIDADES DE VENTA:");
+
+            for (UnidadVenta unidad : festival.getUnidadesDeVenta()) {
+                System.out.println(unidad);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
